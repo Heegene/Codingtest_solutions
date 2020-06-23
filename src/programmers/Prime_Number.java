5648e7c77a3640cc5babc933fc5d427d1a466873
Heegene <dev.heejin@gmail.com>
2020-06-23 14:23:21 +0900

    20023 programmers prime number update

---
+++ src/programmers/Prime_Number.java
@@ -0,0 +1,40 @@
+package programmers;
+
+public class Prime_Number {
+	public static void main(String[] args) {
+		
+		System.out.println(solution(10));
+		
+	}
+	
+	
+	public static int solution(int n) {
+		int answer = 0;
+		int[] number = new int[n+1]; // n보다 1개 많은 배열
+		
+		// 2부터 n까지의 수를 배열에 넣음
+		for (int i = 2; i <= n; i++) {
+			number[i] = i;
+		}
+		
+		// 2부터 시작해서 그 배수들을 0으로 만듦
+		// 0이면 넘어가고 아니면 그 배수들을 다시 0으로 만듦
+		for(int i = 2; i <= n; i++) {
+			if(number[i]==0) { 
+				continue;
+			}
+			for (int j =2*i; j <=n; j+=i) {
+				number[j] = 0;  
+			}
+		}
+		
+		// 배열에서 0이 아닌 것들의 개수를 셈
+		for (int i = 0; i < number.length; i++) {
+			if(number[i]!=0) {
+				answer++;
+			}
+		}
+
+		return answer; 
+}
+}
