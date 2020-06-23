355db87aaef06cd9a03192ec6a121c7625ed9fce
Heegene <dev.heejin@gmail.com>
2020-06-23 12:06:33 +0900

    first commit

---
+++ src/Even_Odd_Number_check.java
@@ -0,0 +1,23 @@
+
+public class Even_Odd_Number_check {
+ public static void main(String[] args) {
+	System.out.println(solution(3));
+	 
+	 
+	 
+	 
+	 
+	 
+}	
+	
+ public static String solution(int num) {
+	 String answer = "";
+	 answer = num % 2 == 0 ? "Even" : "Odd";
+	 return answer;
+ }
+ 
+	
+
+}
+
+
