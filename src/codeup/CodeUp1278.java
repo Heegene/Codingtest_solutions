5dda5bccb94135e616537ea71915ce4f31b2f96c
Heegene <dev.heejin@gmail.com>
2020-08-21 11:59:41 +0900

    200821 Codeup1278 : counting digits

---
+++ src/codeup/CodeUp1278.java
@@ -0,0 +1,15 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1278 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String n = sc.nextLine();
+		// 인트로 받은다음에 변환해도되는데 굳이 그럴필욘X
+		
+		System.out.println(n.length());
+		
+	}
+}
